6af2ca784fed794959edd526d4cd6ad9a17317a3
Sergii Taran <serg.taran1970@gmail.com>
2026-04-06 06:52:57 +0300

    feat: fetch and display notes using react-query

--- src/app/notes/Notes.client.tsx
+++ src/app/notes/Notes.client.tsx
@@ -1,5 +1,31 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+import { fetchNotes } from '@/lib/api';
+
 export default function NotesClient() {
-  return <p>Notes page</p>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['notes'],
+    queryFn: () => fetchNotes(),
+  });
+
+  if (isLoading) {
+    return <p>Loading, please wait...</p>;
+  }
+
+  if (error) {
+    return <p>Something went wrong.</p>;
+  }
+
+  return (
+    <div>
+      <h2>Notes</h2>
+
+      <ul>
+        {data?.notes.map((note) => (
+          <li key={note.id}>{note.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
